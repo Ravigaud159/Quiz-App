6a41b01e56803db6454218f634c870c227f56af3
Ravigaud159 <ravigaud159@gmail.com>
2025-02-04 22:34:39 +0530

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+
+// Top-level bd file where you can add configuration options common to all sub-projects/modules.
+
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+}
